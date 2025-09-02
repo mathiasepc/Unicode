6ba3c94fc71b9e2804d2f737110661006e356600
mathiasepc <mathiasepc@msn.com>
2025-09-02 12:20:42 +0200

    first commit

---
+++ src/main/java/org/example/unicode/UnicodeApplication.java
@@ -0,0 +1,13 @@
+package org.example.unicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnicodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/unicode/UnicodeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.unicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
