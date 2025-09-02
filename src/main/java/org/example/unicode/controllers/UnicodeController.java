69155e6a304d13d2553d691c3cc3dd7e4acd374b
mathiasepc <mathiasepc@msn.com>
2025-09-02 12:27:56 +0200

    Første step lavet

---
+++ src/main/java/org/example/unicode/controllers/UnicodeController.java
@@ -0,0 +1,17 @@
+package org.example.unicode.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/unicode")
+public class UnicodeController {
+
+    @GetMapping("{id}")
+    public String unicodeToChar(@PathVariable int id){
+        char c = (char) id;
+        return "unicode: " + id + " char: " + c;
+    }
+}
---
+++ src/main/java/org/example/unicode/repositories/UnicodeRepository.java
@@ -0,0 +1,8 @@
+//package org.example.unicode.repositories;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface UnicodeRepository extends JpaRepository {
+//
+//
+//}
